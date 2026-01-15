0b324ecfde142aab21f2afc1bd23e0c5efff8e80
laxmi-sikarwar <laxmisikarwar14@gmail.com>
2026-01-15 14:22:03 +0530

    Re-add client-side

---
+++ client-side/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Header from "./components/Header"
+
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
